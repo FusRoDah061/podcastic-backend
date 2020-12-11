2e6177ca50eefcd13126775579bd11c9f045a038
Allex Rodrigues <allexxrodriguess@gmail.com>
2020-12-11 18:44:07 -0300

    fix: MQ connection would not close on healthcheck

--- src/shared/providers/MessagingHealthcheckProvider/implementations/RabbitmqHealthcheckProvider.ts
+++ src/shared/providers/MessagingHealthcheckProvider/implementations/RabbitmqHealthcheckProvider.ts
@@ -6,11 +6,8 @@ export default class RabbitmqHealthcheckProvider
   implements IMessagingHealthcheckProvider {
   public async ping(): Promise<void> {
     try {
-      const channel = await connect(
-        messagingConfig.config.rabbit.url,
-      ).then(conn => conn.createChannel());
-
-      await channel.close();
+      const conn = await connect(messagingConfig.config.rabbit.url);
+      await conn.close();
     } catch (err) {
       throw new Error(`Could not reach rabbitmq database: ${err.message}`);
     }
